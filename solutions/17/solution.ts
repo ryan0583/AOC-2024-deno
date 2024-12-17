09a0834bad2bb1f1f7a0b624227fa2354ba1e099
ryan0583 <ryan0583@gmail.com>
2024-12-17 19:57:56 +0000

    I'm not sure I understand why this works, but it does!

--- solutions/17/solution.ts
+++ solutions/17/solution.ts
@@ -6,52 +6,71 @@ process.stdout.write('\x1Bc'); // Clear the console
 
 const lines = readLines(path.resolve(), 'solutions/17/input.txt');
 
-let regA = Number(lines[0].split(': ')[1]);
-let regB = Number(lines[1].split(': ')[1]);
-let regC = Number(lines[2].split(': ')[1]);
-
 const program = lines[4].split(': ')[1].split(',').map(Number);
 
-const getComboOperand = (literalOperand: number) => {
-  if (literalOperand <= 3) return literalOperand;
-  if (literalOperand === 4) return regA;
-  if (literalOperand === 5) return regB;
-  if (literalOperand === 6) return regC;
-  return 0; // should never happen
+const runProgram = (regA: bigint) => {
+  let regB = BigInt(lines[1].split(': ')[1]);
+  let regC = BigInt(lines[2].split(': ')[1]);
+
+  const getComboOperand = (literalOperand: number) => {
+    if (literalOperand <= 3) return BigInt(literalOperand);
+    if (literalOperand === 4) return regA;
+    if (literalOperand === 5) return regB;
+    if (literalOperand === 6) return regC;
+    return BigInt(0); // should never happen
+  };
+
+  const division = (literalOperand: number) =>
+    regA / 2n ** getComboOperand(literalOperand);
+
+  const output = [] as BigInt[];
+
+  let insNum = 0;
+  while (insNum < program.length) {
+    let shouldIncrement = true;
+    const opcode = program[insNum];
+    const literalOperand = program[insNum + 1];
+    // log(opcode);
+    if (opcode === 0) {
+      regA = division(literalOperand);
+    } else if (opcode === 1) {
+      regB ^= BigInt(literalOperand);
+    } else if (opcode === 2) {
+      regB = getComboOperand(literalOperand) % 8n;
+    } else if (opcode === 3) {
+      if (regA !== 0n) {
+        insNum = literalOperand;
+        shouldIncrement = false;
+      }
+    } else if (opcode === 4) {
+      regB ^= regC;
+    } else if (opcode === 5) {
+      output.push(getComboOperand(literalOperand) % 8n);
+    } else if (opcode === 6) {
+      regB = division(literalOperand);
+    } else if (opcode === 7) {
+      regC = division(literalOperand);
+    }
+    if (shouldIncrement) insNum = insNum + 2;
+  }
+  return output;
 };
 
-const division = (literalOperand: number) =>
-  Math.trunc(regA / Math.pow(2, getComboOperand(literalOperand)));
+log(runProgram(BigInt(lines[0].split(': ')[1])).join(','));
 
-const output = [] as Number[];
+let regA = 0;
+let index = program.length - 1;
 
-let insNum = 0;
-while (insNum < program.length) {
-  let shouldIncrement = true;
-  const opcode = program[insNum];
-  const literalOperand = program[insNum + 1];
-  // log(opcode);
-  if (opcode === 0) {
-    regA = division(literalOperand);
-  } else if (opcode === 1) {
-    regB ^= literalOperand;
-  } else if (opcode === 2) {
-    regB = getComboOperand(literalOperand) % 8;
-  } else if (opcode === 3) {
-    if (regA !== 0) {
-      insNum = literalOperand;
-      shouldIncrement = false;
+while (index >= 0) {
+  const output = runProgram(BigInt(regA)).join(',');
+  if (output === program.slice(index).join(',')) {
+    index--;
+    if (index >= 0) {
+      regA = regA * 8;
     }
-  } else if (opcode === 4) {
-    regB ^= regC;
-  } else if (opcode === 5) {
-    output.push(getComboOperand(literalOperand) % 8);
-  } else if (opcode === 6) {
-    regB = division(literalOperand);
-  } else if (opcode === 7) {
-    regC = division(literalOperand);
+  } else {
+    regA++;
   }
-  if (shouldIncrement) insNum = insNum + 2;
 }
 
-log(output.join(','));
+log(regA);
